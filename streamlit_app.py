b5db70fb187c49d4760b232c549cd1644a189d5f
rjcannizzo <rc.info@protonmail.com>
2023-03-16 13:54:01 -0500

    cleanup multi-select

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avacado'])
 fruits_to_show = my_fruit_list.loc[selected]
 streamlit.dataframe(fruits_to_show)
