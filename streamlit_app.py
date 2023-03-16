5864381ae5eb0d89c1de3fa9ad1c025eecbb0380
rjcannizzo <rc.info@protonmail.com>
2023-03-16 13:10:05 -0500

    emoji test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
+streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Reange Egg')
+
