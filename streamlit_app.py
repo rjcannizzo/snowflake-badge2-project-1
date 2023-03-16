713f856923f8a88aae99a4515fff27b7eb560e16
rjcannizzo <rc.info@protonmail.com>
2023-03-16 12:11:28 -0500

    started app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
+import streamlit
 
-def main():
-    pass
+streamlit.title('My parents new healthy diner')
 
-
-
-if __name__ == '__main__':
-    main()
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Reange Egg')
