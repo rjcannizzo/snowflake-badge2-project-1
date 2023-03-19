b9807387918785947b0797770a61503b02749e7e
rjcannizzo <rc.info@protonmail.com>
2023-03-19 07:14:30 -0500

    testing snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,6 @@ selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),
 fruits_to_show = my_fruit_list.loc[selected]
 streamlit.dataframe(fruits_to_show)
 
-import requests
-
 streamlit.header('Fruityvice Fruit Advice')
 
 choice = streamlit.text_input('What fruit would you like information about?', "Kiwi")
@@ -31,3 +29,10 @@ fruityvice_response = requests.get(f"{BASE_URL}{choice}")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # displays a Pandas dataframe 
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
