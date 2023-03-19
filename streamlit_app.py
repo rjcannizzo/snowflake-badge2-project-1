8f692444c771d10643652af527c1f24ab75b32fa
rjcannizzo <rc.info@protonmail.com>
2023-03-19 09:22:13 -0500

    refactored call to snowflake as function with button

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,15 +36,20 @@ try:
 except URLError as e:
     streamlit.error()
 
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+
+def get_fruit_load_list():
+    with my_cnx.cursor() as cur:
+        cur.execute("select * from fruit_load_list")
+        return cur.fetchall()
+        
+
+if streamlit.button("Get Fuit Load List"):    
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    fruit_data = get_fruit_load_list()
+    streamlit.dataframe(fruit_data)
 
 new_fuit = streamlit.text_input('What fruit would you like to add?')
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 streamlit.stop()
