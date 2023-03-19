397b0533b38b065a562f058a25c31a8855424bb5
rjcannizzo <rc.info@protonmail.com>
2023-03-19 09:07:38 -0500

    added try/except section 12

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,15 +21,17 @@ fruits_to_show = my_fruit_list.loc[selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice')
-
-choice = streamlit.text_input('What fruit would you like information about?', "Kiwi")
 BASE_URL = "https://fruityvice.com/api/fruit/"
-
-fruityvice_response = requests.get(f"{BASE_URL}{choice}")
-# creates a Pandas dataframe by flattening a json object
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# displays a Pandas dataframe 
-streamlit.dataframe(fruityvice_normalized)
+try:
+    choice = streamlit.text_input('What fruit would you like information about?')
+    if not choice:
+        streamlit.error("Please select a fruit to get information.")    
+    else:
+        fruityvice_response = requests.get(f"{BASE_URL}{choice}")       
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())        
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
